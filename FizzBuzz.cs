38d45c033b22d0cecd6b7b2ee3221ab1d24b1f4a
Pier Stoyanov <pier.stoyanov@gmail.com>
2021-06-30 22:58:36 +0300

    First commit, FizzBuzz

---
+++ FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace C_
+{
+    class FizBuzz
+    {
+        static void Main(string[] args)
+        {
+            string numbersLength = Console.ReadLine();
+            for (int i = 1; i < (String.IsNullOrEmpty(numbersLength)? 100 : int.Parse(numbersLength)); i++)
+            {
+                string result = "";
+                if (i % 3 == 0)
+                {
+                    result += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    result += "Buzz";
+                }
+
+                if (result != "") 
+                {
+                    Console.WriteLine(result);
+                }
+                else {
+                    Console.WriteLine(i);              
+                }
+            }
+        }
+    }
+}
