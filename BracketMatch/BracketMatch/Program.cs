7ec4e6e83b173d1a01bef9a572d6ea442c96c24b
Pier Stoyanov <pier.stoyanov@gmail.com>
2021-11-25 23:25:02 +0200

    Added Bracket match console

---
+++ BracketMatch/BracketMatch/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string input = Console.ReadLine();
+string[] exitWords = { "q", "quit", "exit", "esc" };
+
+while (!exitWords.Contains(input) && !System.IO.File.Exists(input))
+{
+    Console.WriteLine("File doesn't exist.");
+    input = Console.ReadLine();
+}
+
+string fileString = System.IO.File.ReadAllText(input);
+
+char[] opening = { '[', '{', '(', '<' };
+char[] closing = { ']', '}', ')', '>' };
+
+Stack<char> bracketStack = new Stack<char>();
+
+bool result = false;
+
+foreach (char c in fileString)
+{
+    if (closing.Contains(c))
+    {
+        var oppositeBracket = opening[Array.IndexOf(closing, c)];
+
+        if (bracketStack.Count != 0 && oppositeBracket == bracketStack.Peek())
+        {
+            result = true;
+            bracketStack.Pop();
+        }
+        else
+        {
+            result = false;
+            break;
+        }
+    }
+
+    if (opening.Contains(c))
+    {
+        bracketStack.Push(c);
+    }
+}
+
+Console.WriteLine(result ? "True, all brackets match" : "False, brackets don't match.");
+return;
